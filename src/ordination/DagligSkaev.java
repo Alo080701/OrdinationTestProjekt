d4a72b480fc462145f6fac463c48c5e5be562d79
Alo080701 <rosette.louise@hotmail.dk>
2024-03-11 11:47:09 +0100

    DagligSkaev

--- src/ordination/DagligSkaev.java
+++ src/ordination/DagligSkaev.java
@@ -6,14 +6,13 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 public class DagligSkaev extends Ordination{
-    private LocalTime[] klokkeslet;
-    private double[] antalEnheder;
+
     private ArrayList<Dosis> doser = new ArrayList<>();
     public DagligSkaev(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel, LocalTime[] klokkeslet, double[] antalEnheder) {
         super(startDen, slutDen, laegemiddel);
-        this.klokkeslet = klokkeslet;
-        this.antalEnheder = antalEnheder;
-        this.doser = doser;
+        for (int i = 0; i < klokkeslet.length; i++) {
+            opretDosis(klokkeslet[i],antalEnheder[i]);
+        }
     }
     // TODO
 ///TEST
