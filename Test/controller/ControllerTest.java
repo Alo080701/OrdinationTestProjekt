d80c82c46aa24c5198d245e9240abe615c5f1f38
Alo080701 <rosette.louise@hotmail.dk>
2024-03-11 10:50:26 +0100

    tests

---
+++ Test/controller/ControllerTest.java
@@ -0,0 +1,52 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    void opretPNOrdination() {
+    }
+
+    @Test
+    void opretDagligFastOrdination() {
+    }
+
+    @Test
+    void opretDagligSkaevOrdination() {
+    }
+
+    @Test
+    void ordinationPNAnvendt() {
+    }
+
+    @Test
+    void anbefaletDosisPrDoegn() {
+    }
+
+    @Test
+    void antalOrdinationerPrVægtPrLægemiddel() {
+    }
+
+    @Test
+    void getAllPatienter() {
+    }
+
+    @Test
+    void getAllLaegemidler() {
+    }
+
+    @Test
+    void opretPatient() {
+    }
+
+    @Test
+    void opretLaegemiddel() {
+    }
+
+    @Test
+    void createSomeObjects() {
+    }
+}
