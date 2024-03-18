4d49c50206cbb433708770091d502120b1ac3eb6
Ufoko <77046870+Ufoko@users.noreply.github.com>
2024-03-18 09:10:48 +0100

    Hovsa

--- Test/controller/ControllerTest.java
+++ Test/controller/ControllerTest.java
@@ -1,10 +1,33 @@
 package controller;
 
+import ordination.Laegemiddel;
+import ordination.Patient;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import storage.Storage;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
+
+    Controller controller;
+    Patient patient20;
+    Patient patient50;
+    Patient patient150;
+    Laegemiddel SjovPille;
+
+
+    @BeforeEach
+    void setup() {
+        controller = Controller.getController();
+
+        patient20 =  controller.opretPatient("0101010101", "Carlsen", 20);
+        patient50 =  controller.opretPatient("0202020202", "Larsen", 50);
+        patient150 = controller.opretPatient("0303030303", "Hansen", 150);
+
+      SjovPille = controller.opretLaegemiddel("Sjov pille", 0.1, 0.15, 0.20, "Styk");
+
+    }
 
     @Test
     void opretPNOrdination() {
@@ -24,6 +47,11 @@ class ControllerTest {
 
     @Test
     void anbefaletDosisPrDoegn() {
+        int expected = 3;
+
+
+        controller.anbefaletDosisPrDoegn(patient20,SjovPille);
+
     }
 
     @Test
