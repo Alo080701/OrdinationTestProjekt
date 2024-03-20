78e871fb1c1628bcb58b06690c0816d2bc7583a4
Alo080701 <rosette.louise@hotmail.dk>
2024-03-20 12:46:30 +0100

    HJEM HJEM HJEM

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -16,7 +16,7 @@ public class DagligFast extends Ordination {
         doser[1] = new Dosis(LocalTime.of(12, 0), middagAntal);
         doser[2] = new Dosis(LocalTime.of(18, 0), aftenAntal);
         doser[3] = new Dosis(LocalTime.of(2, 0), natAntal);
-        if (morgenAntal == 0 && middagAntal == 0 && middagAntal == 0 && aftenAntal == 0){
+        if (morgenAntal == 0 && middagAntal == 0 && aftenAntal == 0 && natAntal == 0){
             throw new ArithmeticException("alle dosis antal kan ikke være 0");
         }
     }
