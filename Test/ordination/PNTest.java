0625b5c986e38a9e0b9063cd62baf46410363e41
Alo080701 <rosette.louise@hotmail.dk>
2024-03-20 11:19:54 +0100

    SKUB SKUB SKUB

--- Test/ordination/PNTest.java
+++ Test/ordination/PNTest.java
@@ -37,22 +37,110 @@ class PNTest {
 
     @Test
     void givDosisT2() {
-        boolean expeceted = true;
+        boolean expected = true;
+        assertEquals(expected, pn1.givDosis(LocalDate.of(2024, 04, 15)));
 
+        ArrayList<LocalDate> expectedDates = new ArrayList<>(List.of(LocalDate.of(2024,04,15)));
+        assertEquals(expectedDates, pn1.getDosisDates());
     }
 
     @Test
     void givDosisT3() {
-        boolean expeceted = true;
+        boolean expected = true;
+        assertEquals(expected, pn1.givDosis(LocalDate.of(2024, 04, 20)));
+
+        ArrayList<LocalDate> expectedDates = new ArrayList<>(List.of(LocalDate.of(2024,04,20)));
+        assertEquals(expectedDates, pn1.getDosisDates());
     }
 
     @Test
     void ugyldigGivDosisT1() {
-        boolean expeceted = false;
+        boolean expected = false;
+        assertEquals(expected, pn1.givDosis(LocalDate.of(2024, 04, 9)));
+
+        ArrayList<LocalDate> expectedDates = new ArrayList<>();
+        assertEquals(expectedDates, pn1.getDosisDates());
     }
 
     @Test
     void ugyldigGivDosisT2() {
-        boolean expeceted = false;
+        boolean expected = false;
+        assertEquals(expected, pn1.givDosis(LocalDate.of(2024, 04, 21)));
+
+        ArrayList<LocalDate> expectedDates = new ArrayList<>();
+        assertEquals(expectedDates, pn1.getDosisDates());
     }
+
+    @Test
+    void getAntalGangeGivetT1() {
+        pn1.givDosis(LocalDate.of(2024, 04, 10));
+
+        int expected = 1;
+
+        assertEquals(expected, pn1.getAntalGangeGivet());
+
+    }
+    @Test
+    void getAntalGangeGivetT2() {
+        pn1.givDosis(LocalDate.of(2024, 04, 10));
+        pn1.givDosis(LocalDate.of(2024, 04, 11));
+
+        int expected = 2;
+
+        assertEquals(expected, pn1.getAntalGangeGivet());
+    }
+    @Test
+    void getAntalGangeGivetT3() {
+        pn1.givDosis(LocalDate.of(2024, 04, 10));
+        pn1.givDosis(LocalDate.of(2024, 04, 11));
+        pn1.givDosis(LocalDate.of(2024, 05, 10));
+
+        int expected = 2;
+
+        assertEquals(expected, pn1.getAntalGangeGivet());
+    }
+
+    @Test
+    void doegnDosisT1() {
+        pn1.givDosis(LocalDate.of(2024, 04, 15));
+
+        double expected = 2;
+
+        assertEquals(expected, pn1.doegnDosis());
+
+    }
+    @Test
+    void doegnDosisT2() {
+        pn1.givDosis(LocalDate.of(2024, 04, 10));
+        pn1.givDosis(LocalDate.of(2024, 04, 20));
+
+        double expected = 0.4;
+
+        assertEquals(expected, pn1.doegnDosis());
+
+    }
+    @Test
+    void doegnDosisT3() {
+        pn1.givDosis(LocalDate.of(2024, 04, 10));
+        pn1.givDosis(LocalDate.of(2024, 04, 15));
+        pn1.givDosis(LocalDate.of(2024, 04, 20));
+
+        double expected = 0.6;
+
+        assertEquals(expected, pn1.doegnDosis());
+
+    }
+    @Test
+    void doegnDosisT4() {
+        pn1.givDosis(LocalDate.of(2024, 04, 10));
+        pn1.givDosis(LocalDate.of(2024, 04, 15));
+        pn1.givDosis(LocalDate.of(2024, 04, 16));
+        pn1.givDosis(LocalDate.of(2024, 04, 20));
+
+        double expected = 0.8;
+
+        assertEquals(expected, pn1.doegnDosis());
+
+    }
+
 }
